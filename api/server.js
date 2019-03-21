6bcd5241cdf2f445681c1ed7a8dae807d06ea955
Thorben Bender <bender.thorben0@gmail.com>
2019-03-21 11:40:58 +0100

    added react

--- api/server.js
+++ api/server.js
@@ -1,33 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
+const path = require('path');
 
+const pathToBuildFolder = path.join(__dirname, '..', 'eu1-cra', 'build');
+const pathToIndexHtml = path.join(__dirname, '..', 'eu1-cra', 'build', 'index.html');
+console.log(pathToIndexHtml);
 const db = require('../data/db.js');
 
 const server = express();
 
 server.use(helmet());
-server.use(express.json());
+server.use(express.static(pathToBuildFolder));
 
-server.get('/', async (req, res) => {
-  try {
-    const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  }
-});
-
-server.post('/', async (req, res) => {
-  try {
-    const [id] = await db('shoutouts').insert(req.body);
-    const shoutouts = await db('shoutouts');
-
-    res.status(201).json(shoutouts);
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
-  }
+server.get('/', (req, res) => {
+	res.sendFile(pathToIndexHtml);
 });
 
 module.exports = server;
